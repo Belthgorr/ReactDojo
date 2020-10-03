14530a35e37051fc4a087913b2f1366d99d8d638
victor.mora <vdaniel.mora@udea.edu.co>
2020-10-03 09:36:30 -0500

    dojo

---
+++ components/ActionPokemon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+//import { BuyPokemonActions, ReturnPokemonActions } from '../redux/action'
+
+export default function ActionPokemon() {
+    const dispatch = useDispatch()
+    return (
+        <div>
+         <button className="btn btn-dark btn-sm mb-2" onClick={() => {
+     dispatch(BuyPokemonActions(1));
+     }}>Comprar Pokemon</button>
+      <button className="btn btn-dark btn-sm " onClick={() => {
+         dispatch(ReturnPokemonActions(1))
+        }}>Devolver Pokemon</button>
+        </div>
+    )
+
+}
